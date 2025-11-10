b765116b177ada98de7dfa807a551817decd7939
Aymen khlil <aymen-khlil@outlook.com>
2025-11-11 00:55:18 +0100

    Clean ups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,39 @@
-import MagicButton from "./components/button/MagicButton";
+import Shuffle from "@/components/Shuffle";
+import MagicButton from "../components/button/MagicButton";
+import TextType from "@/components/TextType";
 
 export default function HomePage() {
   return (
     <section className="h-screen flex flex-col items-center justify-center text-center overflow-hidden ">
       <div className=" flex items-center justify-center h-full w-full">
-        <div className="flex flex-col h-screen items-center justify-around ">
-          <p className="title text-7xl text-center flex items-center justify-center animate-typing">
-            WELCOME WIZARD
-          </p>
-          <MagicButton />
+        <div
+          className="flex flex-col h-screen items-center justify-between"
+          style={{ padding: "70px 0px" }}
+        >
+          <div className="">
+            <div>
+              <TextType
+                text={"WELCOME"}
+                typingSpeed={75}
+                pauseDuration={1000}
+                showCursor={false}
+                className="title text-3xl sm:text-4xl md:text-6xl animate-typing"
+              />
+            </div>
+            <div className=" ">
+              <TextType
+                text={"WIZARD"}
+                typingSpeed={75}
+                pauseDuration={1500}
+                initialDelay={1070}
+                showCursor={false}
+                className="title text-5xl sm:text-6xl md:text-8xl  animate-typing "
+              />
+            </div>
+          </div>
+          <div className="">
+            <MagicButton />
+          </div>
         </div>
       </div>
     </section>
--- src/components/button/MagicButton.tsx
+++ src/components/button/MagicButton.tsx
@@ -4,17 +4,10 @@ import Image from "next/image";
 import "./button.css";
 
 export default function MagicButton() {
-  const handleClick = () => {
-    // Later you can navigate with useRouter() to next page (e.g. /character-select)
-    alert("Your quest begins...");
-  };
-
   return (
     <div className="flex flex-col items-center justify-center ">
       <Image src="/aaa.png" width={200} height={100} alt="wizzardCap" />
-      <button className="pixel-button text-sm  bg-[#511113] w-80">
-        Start Quest
-      </button>
+      <button className="pixel-btn text-sm w-80 ">Start Quest</button>
     </div>
   );
 }
